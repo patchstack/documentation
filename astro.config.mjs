8cba3776651991673add055d640921f0a371375e
maciekpalmowski <136691573+maciekpalmowski@users.noreply.github.com>
2024-02-23 02:25:11 +0100

    OpenGraph images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+const site_url = process.env.URL;
+
+const site = site_url || 'http://localhost:4321';
 // https://astro.build/config
 export default defineConfig({
+	site,
 	integrations: [
 		starlight({
 			title: 'Patchstack Docs',
---
+++ src/pages/open-graph/[...route].js
@@ -0,0 +1,44 @@
+import { OGImageRoute } from 'astro-og-canvas';
+import { getCollection } from 'astro:content';
+
+const collectionEntries = await getCollection('docs');
+const pages = Object.fromEntries(collectionEntries.map(({ slug, data }) => [slug, data]));
+
+export const { getStaticPaths, GET } = OGImageRoute({
+    param: 'route',
+
+    pages: pages,
+
+    getImageOptions: (path, page) => ({
+        title: page.ogtitle ? page.ogtitle : page.title,
+        description: 'Detect & patch vulnerabilities in your WordPress apps',
+        bgImage: {
+            path: './public/images/og_bg.png',
+            fit: 'contain'
+        },
+        logo: {
+            path: './public/images/patchstack_logomark.png',
+            size:[100]
+        },
+        font: {
+            title: {
+                size: 65,
+                families: [
+                    'Faktum Wide SemiBold',
+                ],
+                weight: 'SemiBold',
+            },
+            description: {
+                size: 25,
+                lineHeight: 1.25,
+                families: [
+                    'Faktum Wide SemiBold',
+                ],
+                weight: 'regular',
+            },
+        },
+        fonts: [
+            './src/pages/open-graph/_faktum.woff',
+        ],
+    }),
+});
