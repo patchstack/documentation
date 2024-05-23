64a55691f45810eb24acccc3ee2438d9ae4c90d0
maciekpalmowski <136691573+maciekpalmowski@users.noreply.github.com>
2024-05-23 23:24:48 +0200

    Adds sitemap support

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ const site_url = process.env.URL;
 const site = site_url || 'http://localhost:4321';
 // https://astro.build/config
 export default defineConfig({
-	site,
+	site: site,
 	image: {
 		service: passthroughImageService(),
 	},
